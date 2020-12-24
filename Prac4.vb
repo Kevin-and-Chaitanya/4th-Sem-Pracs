a5d9cc9145152469c8b51a81470d62612aa60969
ChaitanyaParmar <68532604+ChaitanyaParmar@users.noreply.github.com>
2020-12-24 16:07:47 +0530

    Add files via upload

---
+++ Prac4.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Console
+Module Prac4
+    Sub main()
+        Dim a, b, choice As Integer
+        WriteLine("Enter number 1:")
+        a = CInt(ReadLine())
+        WriteLine("Enter number 2:")
+        b = CInt(ReadLine())
+
+        WriteLine("1. Addition, 2. Subtraction, 3. Division, 4. Multiplication, 5. Square, 6.Cube")
+        choice = CInt(ReadLine())
+
+        If choice = 1 Then
+            WriteLine("Ans is {0}", a + b)
+        ElseIf choice = 2 Then
+            WriteLine("Ans is {0}", a - b)
+        ElseIf choice = 3 Then
+            WriteLine("Ans is {0}", a / b)
+        ElseIf choice = 4 Then
+            WriteLine("Ans is {0}", a * b)
+        ElseIf choice = 5 Then
+            WriteLine("Ans is {0}", a * a)
+        Else choice = 6
+            WriteLine("Ans is {0}", b * b * b)
+        End If
+    End Sub
+End Module
