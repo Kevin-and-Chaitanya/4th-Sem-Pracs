957952df98eb01ff268d55d5610bdb6cbdfde010
ChaitanyaParmar <68532604+ChaitanyaParmar@users.noreply.github.com>
2021-01-12 11:29:25 +0530

    Update Prac06.vb

--- Prac06.vb
+++ Prac06.vb
@@ -1,9 +1,10 @@
-﻿Imports System.Console
-Module Prac06
-    Sub main()
-        Dim a, b, choice As Integer
+Imports System.Console
+Module Practical_06
 
-        WriteLine("1. Addition, 2. Subtraction, 3. Division, 4. Multiplication, 5. Square, 6.Cube")
+    Sub Main()
+        Dim a, b, choice As Integer
+repeat:
+        WriteLine("1. Addition, 2. Subtraction, 3. Division, 4. Multiplication, 5. Exponential, 6.Modulo, 7.Exit")
         choice = CInt(ReadLine())
 
         If choice = 1 Then
@@ -12,37 +13,55 @@ Module Prac06
             WriteLine("Enter number 2:")
             b = CInt(ReadLine())
             WriteLine("Ans is {0}", a + b)
-
+            Write(vbNewLine)
+            GoTo repeat
         ElseIf choice = 2 Then
             WriteLine("Enter number 1:")
             a = CInt(ReadLine())
             WriteLine("Enter number 2:")
             b = CInt(ReadLine())
             WriteLine("Ans is {0}", a - b)
-
+            Write(vbNewLine)
+            GoTo repeat
         ElseIf choice = 3 Then
             WriteLine("Enter number 1:")
             a = CInt(ReadLine())
             WriteLine("Enter number 2:")
             b = CInt(ReadLine())
             WriteLine("Ans is {0}", a / b)
-
+            Write(vbNewLine)
+            GoTo repeat
         ElseIf choice = 4 Then
             WriteLine("Enter number 1:")
             a = CInt(ReadLine())
             WriteLine("Enter number 2:")
             b = CInt(ReadLine())
             WriteLine("Ans is {0}", a * b)
-
+            Write(vbNewLine)
+            GoTo repeat
         ElseIf choice = 5 Then
             WriteLine("Enter number 1:")
             a = CInt(ReadLine())
-            WriteLine("Ans is {0}", a * a)
-
-        Else choice = 6
             WriteLine("Enter number 2:")
             b = CInt(ReadLine())
-            WriteLine("Ans is {0}", b * b * b)
+            WriteLine("Ans is {0}", a ^ b)
+            Write(vbNewLine)
+            GoTo repeat
+        ElseIf choice = 6 Then
+            WriteLine("Enter number 1:")
+            a = CInt(ReadLine())
+            WriteLine("Enter number 2:")
+            b = CInt(ReadLine())
+            WriteLine("Ans is {0}", a Mod b)
+            Write(vbNewLine)
+            GoTo repeat
+        ElseIf choice = 7 Then
+            GoTo xx
+        Else
+            WriteLine("Wrong input!!")
+            Write(vbNewLine)
         End If
+        Read()
+xx:
     End Sub
 End Module
